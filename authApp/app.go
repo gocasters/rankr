89fdfcc078eab084e1ca56e9fa1b273c1d3245ba
leylahosseini <slaila.hosseini@gmail.com>
2025-08-21 19:45:23 +0330

    add repository -2

--- authApp/app.go
+++ authApp/app.go
@@ -11,20 +11,40 @@ import (
     "github.com/labstack/echo/v4/middleware"
     "time"
     "log"
+    "os"
+    "strconv"
 )
 
 func main() {
     cfg := config.LoadConfig()
 
-	dbConfig := database.NewConfig(
-		database.WithHost("127.0.0.1"),
-		database.WithPort(5432),
-		database.WithUsername("leyla"),
-		database.WithPassword("123456"),
-		database.WithDBName("authdb"),
-		database.WithSSLMode("disable"),
-	)
-
+        host := os.Getenv("DB_HOST")
+        if host == "" {
+            host = "127.0.0.1"
+        }
+        port := 5432
+        if p := os.Getenv("DB_PORT"); p != "" {
+            if v, err := strconv.Atoi(p); err == nil && v > 0 {
+                port = v
+            }
+        }
+        user := os.Getenv("DB_USER")
+        pass := os.Getenv("DB_PASSWORD")
+        dbname := os.Getenv("DB_NAME")
+        sslmode := os.Getenv("DB_SSLMODE")
+        if sslmode == "" {
+            sslmode = "disable"
+        }
+    
+        dbConfig := database.NewConfig(
+            database.WithHost(host),
+            database.WithPort(port),
+            database.WithUsername(user),
+            database.WithPassword(pass),
+            database.WithDBName(dbname),
+            database.WithSSLMode(sslmode),
+       )
+  
 	// 
 	db, err := database.Connect(dbConfig)
 	if err != nil {
