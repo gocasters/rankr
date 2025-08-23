35b39b1733452acec29671054512b449ad525fa4
mehrdadbn9 <m.biukian@tapsell.ir>
2025-08-23 07:26:44 +0330

    fixed coderabit recomendation

--- pkg/err_msg/err_msg.go
+++ pkg/err_msg/err_msg.go
@@ -3,19 +3,19 @@ package errmsg
 import "errors"
 
 var (
-	ErrValidationFailed     = errors.New("input validation failed")
-	ErrUnexpectedError      = errors.New("unexpected error occurred")
-	ErrInvalidRequestFormat = errors.New("invalid request format")
-	ErrGetUserInfo          = errors.New("get user info failed")
-	ErrFailedDecodeBase64   = errors.New("decode data to base 64 failed")
-	ErrFailedUnmarshalJson  = errors.New("unmarshal data to JSON failed")
-	ErrUnauthorized         = errors.New("unauthorized")
+    ErrValidationFailed     = errors.New("input validation failed")
+    ErrUnexpectedError      = errors.New("unexpected error occurred")
+    ErrInvalidRequestFormat = errors.New("invalid request format")
+    ErrGetUserInfo          = errors.New("get user info failed")
+    ErrFailedDecodeBase64   = errors.New("decode data to base 64 failed")
+    ErrFailedUnmarshalJSON  = errors.New("unmarshal data to JSON failed")
+    ErrUnauthorized         = errors.New("unauthorized")
 )
 
 // Define constant messages generally
 const (
-	MessageMissingXUserData  = "Missing X-User-Info header"
-	MessageInvalidBase64     = "Invalid Base64 data"
-	MessageInvalidJsonFormat = "Invalid JSON format"
-	ServerError              = "Internal server error"
+    MessageMissingXUserData  = "Missing X-User-Info header"
+    MessageInvalidBase64     = "Invalid Base64 data"
+    MessageInvalidJSONFormat = "Invalid JSON format"
+    ServerError              = "Internal server error"
 )
