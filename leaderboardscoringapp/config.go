1322e81a1368ade9925045403cba82ff2d03f695
Mohammad Reza Javid <mohammadrezajavid60@gmail.com>
2025-08-31 20:18:26 +0330

    add simple scheduler by time.ticker, and refactor app.go, config, readme

--- leaderboardscoringapp/config.go
+++ leaderboardscoringapp/config.go
@@ -3,6 +3,8 @@ package leaderboardscoringapp
 import (
 	"github.com/gocasters/rankr/adapter/redis"
 	"github.com/gocasters/rankr/leaderboardscoringapp/delivery/consumer"
+	"github.com/gocasters/rankr/leaderboardscoringapp/delivery/scheduler"
+	"github.com/gocasters/rankr/leaderboardscoringapp/service/leaderboardscoring"
 	"github.com/gocasters/rankr/pkg/database"
 	"github.com/gocasters/rankr/pkg/httpserver"
 	"github.com/gocasters/rankr/pkg/logger"
@@ -10,12 +12,14 @@ import (
 )
 
 type Config struct {
-	HTTPServer           httpserver.Config `koanf:"http_server"`
-	PostgresDB           database.Config   `koanf:"postgres_db"`
-	Redis                redis.Config      `koanf:"redis"`
-	Logger               logger.Config     `koanf:"logger"`
-	Consumer             consumer.Config   `koanf:"consumer"`
-	TotalShutdownTimeout time.Duration     `koanf:"total_shutdown_timeout"`
-	PathOfMigration      string            `koanf:"path_of_migration"`
-	SubscriberTopic      string            `koanf:"subscriber_topic"`
+	HTTPServer           httpserver.Config         `koanf:"http_server"`
+	Scheduler            scheduler.Config          `koanf:"scheduler"`
+	LeaderboardScoring   leaderboardscoring.Config `koanf:"leaderboard_scoring"`
+	PostgresDB           database.Config           `koanf:"postgres_db"`
+	Redis                redis.Config              `koanf:"redis"`
+	Logger               logger.Config             `koanf:"logger"`
+	Consumer             consumer.Config           `koanf:"consumer"`
+	TotalShutdownTimeout time.Duration             `koanf:"total_shutdown_timeout"`
+	PathOfMigration      string                    `koanf:"path_of_migration"`
+	SubscriberTopic      string                    `koanf:"subscriber_topic"`
 }
