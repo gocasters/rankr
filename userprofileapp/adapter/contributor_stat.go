f86b4a921afe9c0de33b56c923c8ae98a2d2bb65
mzfarshad <farshad.akbari.arzati@gmail.com>
2025-10-04 23:37:20 +0330

    Fix code rabbit ai suggestions

--- userprofileapp/adapter/contributor_stat.go
+++ userprofileapp/adapter/contributor_stat.go
@@ -15,5 +15,5 @@ func newContributorStatRPC() contributorStatRPC {
 func (a contributorStatRPC) GetContributorStat(ctx context.Context, userID int64) (userprofile.ContributorStat, error) {
 	// TODO: Implement me
 
-	return userprofile.ContributorStat{}, fmt.Errorf("implement me. userprofileapp/adapter/contributor_rpc.go GetContributorStat")
+	return userprofile.ContributorStat{}, fmt.Errorf("implement me. userprofileapp/adapter/contributor_stat.go GetContributorStat")
 }
