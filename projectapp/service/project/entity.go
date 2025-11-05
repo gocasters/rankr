c82a50280112c5c469a8a5b3e70076ded2d4ea45
Arash <75931882+arash-mosavi@users.noreply.github.com>
2025-11-05 08:20:45 +0330

    feat(project): add git repository fields and refactor project status lifecycle (#172)
    * feat(project): add git repository fields and refactor project status handling
    
    * refactor(param, service, validator): simplify pointer usage for project parameters

--- projectapp/service/project/entity.go
+++ projectapp/service/project/entity.go
@@ -12,6 +12,8 @@ type ProjectEntity struct {
 	Slug               string                 `db:"slug" json:"slug"`
 	Description        *string                `db:"description" json:"description,omitempty"`
 	DesignReferenceURL *string                `db:"design_reference_url" json:"designReferenceUrl,omitempty"`
+	GitRepoID          *string                `db:"git_repo_id" json:"gitRepoId,omitempty"`
+	RepoProvider       *constant.VcsProvider  `db:"repo_provider" json:"repoProvider,omitempty"`
 	Status             constant.ProjectStatus `db:"status" json:"status"`
 	CreatedAt          time.Time              `db:"created_at" json:"createdAt"`
 	UpdatedAt          time.Time              `db:"updated_at" json:"updatedAt"`
