1bc93345e6da1d345a587fa0bb29734e36e329a7
mzfarshad <farshad.akbari.arzati@gmail.com>
2025-11-07 21:34:43 +0330

    refactor validation, service layer and repo files

--- notifapp/delivery/http/handler.go
+++ notifapp/delivery/http/handler.go
@@ -1,36 +1,221 @@
 package http
 
 import (
+	"errors"
 	"github.com/gocasters/rankr/notifapp/service/notification"
+	errmsg "github.com/gocasters/rankr/pkg/err_msg"
+	"github.com/gocasters/rankr/pkg/statuscode"
+	"github.com/gocasters/rankr/pkg/validator"
 	types "github.com/gocasters/rankr/type"
 	"github.com/labstack/echo/v4"
+	"net/http"
 	"strconv"
 )
 
-var (
-	userID         = "user_id"
-	notificationID = "notification_id"
-)
+var notificationID = "notification_id"
 
 type Handler struct {
-	validator notification.Validate
-	service   notification.Service
+	service notification.Service
 }
 
-func NewHandler(svc notification.Service, validate notification.Validate) Handler {
-	return Handler{validator: validate, service: svc}
+func NewHandler(svc notification.Service) Handler {
+	return Handler{service: svc}
 }
 
-func getID(c echo.Context, param string) (types.ID, error) {
-	idStr := c.Param(param)
+func getNotifID(c echo.Context) (types.ID, error) {
+
+	idStr := c.Param(notificationID)
 	if idStr == "" {
-		return 0, echo.ErrBadRequest
+		return 0, errors.New("invalid notification id in param")
 	}
 
 	id, err := strconv.ParseUint(idStr, 10, 64)
 	if err != nil {
-		return 0, echo.ErrBadRequest
+		return 0, errors.New("invalid notification id in param")
 	}
 
 	return types.ID(id), nil
+}
+
+func getUserID(c echo.Context) types.ID {
+	return c.Get("userInfo").(types.UserClaim).ID
+}
+
+func (h Handler) createNotification(c echo.Context) error {
+
+	var req notification.CreateRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.ErrBadRequest)
+	}
+
+	req.UserID = getUserID(c)
+
+	res, err := h.service.Create(c.Request().Context(), req)
+	if err != nil {
+		if vErr, ok := err.(validator.Error); ok {
+			return c.JSON(vErr.StatusCode(), vErr)
+		}
+
+		if eRes, ok := err.(errmsg.ErrorResponse); ok {
+			return c.JSON(statuscode.MapToHTTPStatusCode(eRes), eRes)
+		}
+
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusCreated, res)
+}
+
+func (h Handler) deleteNotification(c echo.Context) error {
+
+	var req notification.DeleteRequest
+
+	req.UserID = getUserID(c)
+
+	notifyID, err := getNotifID(c)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	req.NotificationID = notifyID
+
+	err = h.service.Delete(c.Request().Context(), req)
+	if err != nil {
+		if vErr, ok := err.(validator.Error); ok {
+			return c.JSON(vErr.StatusCode(), vErr)
+		}
+
+		if eRes, ok := err.(errmsg.ErrorResponse); ok {
+			return c.JSON(statuscode.MapToHTTPStatusCode(eRes), eRes)
+		}
+
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
+
+func (h Handler) markAsRead(c echo.Context) error {
+
+	var req notification.MarkAsReadRequest
+
+	req.UserID = getUserID(c)
+
+	notifyID, err := getNotifID(c)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	req.NotificationID = notifyID
+
+	res, err := h.service.MarkAsRead(c.Request().Context(), req)
+	if err != nil {
+		if vErr, ok := err.(validator.Error); ok {
+			return c.JSON(vErr.StatusCode(), vErr)
+		}
+
+		if eRes, ok := err.(errmsg.ErrorResponse); ok {
+			return c.JSON(statuscode.MapToHTTPStatusCode(eRes), eRes)
+		}
+
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, res)
+}
+
+func (h Handler) markAllAsRead(c echo.Context) error {
+
+	var req notification.MarkAllAsReadRequest
+
+	req.UserID = getUserID(c)
+
+	err := h.service.MarkAllAsRead(c.Request().Context(), req)
+	if err != nil {
+		if vErr, ok := err.(validator.Error); ok {
+			return c.JSON(vErr.StatusCode(), vErr)
+		}
+
+		if eRes, ok := err.(errmsg.ErrorResponse); ok {
+			return c.JSON(statuscode.MapToHTTPStatusCode(eRes), eRes)
+		}
+
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{"message": "Successfully done all notifications as read"})
+}
+
+func (h Handler) getNotification(c echo.Context) error {
+
+	var req notification.GetRequest
+
+	notifyID, err := getNotifID(c)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	req.NotificationID = notifyID
+	req.UserID = getUserID(c)
+
+	res, err := h.service.Get(c.Request().Context(), req)
+	if err != nil {
+		if vErr, ok := err.(validator.Error); ok {
+			return c.JSON(vErr.StatusCode(), vErr)
+		}
+
+		if eRes, ok := err.(errmsg.ErrorResponse); ok {
+			return c.JSON(statuscode.MapToHTTPStatusCode(eRes), eRes)
+		}
+
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, res)
+}
+
+func (h Handler) listNotification(c echo.Context) error {
+
+	var req notification.ListRequest
+
+	req.UserID = getUserID(c)
+
+	res, err := h.service.List(c.Request().Context(), req)
+	if err != nil {
+		if vErr, ok := err.(validator.Error); ok {
+			return c.JSON(vErr.StatusCode(), vErr)
+		}
+
+		if eRes, ok := err.(errmsg.ErrorResponse); ok {
+			return c.JSON(statuscode.MapToHTTPStatusCode(eRes), eRes)
+		}
+
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, res)
+}
+
+func (h Handler) getUnreadCount(c echo.Context) error {
+
+	var req notification.CountUnreadRequest
+
+	req.UserID = getUserID(c)
+
+	res, err := h.service.GetUnreadCount(c.Request().Context(), req)
+	if err != nil {
+		if vErr, ok := err.(validator.Error); ok {
+			return c.JSON(vErr.StatusCode(), vErr)
+		}
+
+		if eRes, ok := err.(errmsg.ErrorResponse); ok {
+			return c.JSON(statuscode.MapToHTTPStatusCode(eRes), eRes)
+		}
+
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, res)
 }
--- notifapp/service/notification/validation.go
+++ notifapp/service/notification/validation.go
@@ -1,14 +1,9 @@
 package notification
 
 import (
-	"errors"
 	"fmt"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	types "github.com/gocasters/rankr/type"
-)
-
-var (
-	ErrMsgInvalidInput = errors.New("invalid input")
 )
 
 type Validate struct{}
@@ -17,28 +12,11 @@ func NewValidation() Validate {
 	return Validate{}
 }
 
-func (v Validate) CreateNotificationValidate(req CreateRequest) (map[string]string, error) {
-
-	if err := validation.ValidateStruct(&req,
+func (v Validate) CreateNotificationValidate(req CreateRequest) error {
+	return validation.ValidateStruct(&req,
 		validation.Field(&req.UserID, validation.Required, validation.By(validID)),
 		validation.Field(&req.Message, validation.Required),
-		validation.Field(&req.Type, validation.Required, validation.By(validNotifyType)),
-	); err != nil {
-		fieldErr := make(map[string]string)
-		errV, ok := err.(validation.Errors)
-
-		if ok {
-			for key, value := range errV {
-				if value != nil {
-					fieldErr[key] = value.Error()
-				}
-			}
-		}
-
-		return fieldErr, ErrMsgInvalidInput
-	}
-
-	return nil, nil
+		validation.Field(&req.Type, validation.Required, validation.By(validNotifyType)))
 }
 
 func validID(value interface{}) error {
@@ -59,137 +37,36 @@ func validNotifyType(value interface{}) error {
 	return nil
 }
 
-func (v Validate) GetNotificationValidate(req GetRequest) (map[string]string, error) {
+func (v Validate) GetNotificationValidate(req GetRequest) error {
+	return validation.ValidateStruct(&req,
+		validation.Field(&req.UserID, validation.Required, validation.By(validID)),
+		validation.Field(&req.NotificationID, validation.Required, validation.By(validID)))
+}
 
-	if err := validation.ValidateStruct(&req,
+func (v Validate) ListNotificationsValidate(req ListRequest) error {
+	return validation.ValidateStruct(&req,
+		validation.Field(&req.UserID, validation.Required, validation.By(validID)))
+}
+
+func (v Validate) MarkAsReadNotificationValidate(req MarkAsReadRequest) error {
+	return validation.ValidateStruct(&req,
+		validation.Field(&req.UserID, validation.Required, validation.By(validID)),
+		validation.Field(&req.NotificationID, validation.Required, validation.By(validID)))
+}
+
+func (v Validate) MarkAllAsReadNotificationValidate(req MarkAllAsReadRequest) error {
+	return validation.ValidateStruct(&req,
+		validation.Field(&req.UserID, validation.Required, validation.By(validID)))
+}
+
+func (v Validate) DeleteNotificationValidate(req DeleteRequest) error {
+	return validation.ValidateStruct(&req,
 		validation.Field(&req.UserID, validation.Required, validation.By(validID)),
 		validation.Field(&req.NotificationID, validation.Required, validation.By(validID)),
-	); err != nil {
-		fieldErr := make(map[string]string)
-
-		errV, ok := err.(validation.Errors)
-		if ok {
-			for key, value := range errV {
-				if value != nil {
-					fieldErr[key] = value.Error()
-				}
-			}
-		}
-
-		return fieldErr, ErrMsgInvalidInput
-	}
-
-	return nil, nil
+	)
 }
 
-func (v Validate) ListNotificationsValidate(req ListRequest) (map[string]string, error) {
-
-	if err := validation.ValidateStruct(&req,
-		validation.Field(&req.UserID, validation.Required, validation.By(validID)),
-	); err != nil {
-		fieldErr := make(map[string]string)
-
-		errV, ok := err.(validation.Errors)
-		if ok {
-			for key, value := range errV {
-				if value != nil {
-					fieldErr[key] = value.Error()
-				}
-			}
-		}
-
-		return fieldErr, ErrMsgInvalidInput
-	}
-
-	return nil, nil
-}
-
-func (v Validate) MarkAsReadNotificationValidate(req MarkAsReadRequest) (map[string]string, error) {
-
-	if err := validation.ValidateStruct(&req,
-		validation.Field(&req.UserID, validation.Required, validation.By(validID)),
-		validation.Field(&req.NotificationID, validation.Required, validation.By(validID)),
-	); err != nil {
-		fieldErr := make(map[string]string)
-
-		errV, ok := err.(validation.Errors)
-		if ok {
-			for key, value := range errV {
-				if value != nil {
-					fieldErr[key] = value.Error()
-				}
-			}
-		}
-
-		return fieldErr, ErrMsgInvalidInput
-	}
-
-	return nil, nil
-}
-
-func (v Validate) MarkAllAsReadNotificationValidate(req MarkAllAsReadRequest) (map[string]string, error) {
-
-	if err := validation.ValidateStruct(&req,
-		validation.Field(&req.UserID, validation.Required, validation.By(validID)),
-	); err != nil {
-		fieldErr := make(map[string]string)
-
-		errV, ok := err.(validation.Errors)
-		if ok {
-			for key, value := range errV {
-				if value != nil {
-					fieldErr[key] = value.Error()
-				}
-			}
-		}
-
-		return fieldErr, ErrMsgInvalidInput
-	}
-
-	return nil, nil
-}
-
-func (v Validate) DeleteNotificationValidate(req DeleteRequest) (map[string]string, error) {
-
-	if err := validation.ValidateStruct(&req,
-		validation.Field(&req.UserID, validation.Required, validation.By(validID)),
-		validation.Field(&req.NotificationID, validation.Required, validation.By(validID)),
-	); err != nil {
-		fieldErr := make(map[string]string)
-
-		errV, ok := err.(validation.Errors)
-		if ok {
-			for key, value := range errV {
-				if value != nil {
-					fieldErr[key] = value.Error()
-				}
-			}
-		}
-
-		return fieldErr, ErrMsgInvalidInput
-	}
-
-	return nil, nil
-}
-
-func (v Validate) GetUnreadCountNotificationValidate(req CountUnreadRequest) (map[string]string, error) {
-
-	if err := validation.ValidateStruct(&req,
-		validation.Field(&req.UserID, validation.Required, validation.By(validID)),
-	); err != nil {
-		fieldErr := make(map[string]string)
-
-		errV, ok := err.(validation.Errors)
-		if ok {
-			for key, value := range errV {
-				if value != nil {
-					fieldErr[key] = value.Error()
-				}
-			}
-		}
-
-		return fieldErr, ErrMsgInvalidInput
-	}
-
-	return nil, nil
+func (v Validate) GetUnreadCountNotificationValidate(req CountUnreadRequest) error {
+	return validation.ValidateStruct(&req,
+		validation.Field(&req.UserID, validation.Required, validation.By(validID)))
 }
