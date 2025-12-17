2f79e207566a0eb27d05ebecafe90cb42b0d8bb3
arash.mousavi <arashmousavi8131@gmail.com>
2025-12-17 09:16:39 +0330

    fix: enhance logging for event handling with structured logging

--- webhookapp/service/delivery/service.go
+++ webhookapp/service/delivery/service.go
@@ -3,6 +3,8 @@ package delivery
 import (
 	"context"
 	"fmt"
+	"log/slog"
+
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/gocasters/rankr/pkg/logger"
@@ -106,7 +108,7 @@ func (s *Service) ProcessBatch(ctx context.Context) error {
 		return nil
 	}
 
-	logger.L().Debug("row events", events)
+	logger.L().Debug("row events", slog.Any("events", events))
 
 	savedEvents, err := s.repo.BulkInsertPostgresSQL(ctx, events)
 	if err != nil {
@@ -114,7 +116,7 @@ func (s *Service) ProcessBatch(ctx context.Context) error {
 		return fmt.Errorf("bulk insert failed: %w", err)
 	}
 
-	logger.L().Debug("events", savedEvents)
+	logger.L().Debug("events", slog.Any("savedEvents", savedEvents))
 
 	for _, ev := range savedEvents {
 		payload, err := proto.Marshal(ev)
